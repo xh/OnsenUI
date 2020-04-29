3c62d96495d48e7bf08c24b1b2bb8a78a28a3a4a
Tom Tirapani <hello@tomtirapani.com>
2020-04-29 13:37:07 +0100

    Ensure @xh/react-onsenui uses @xh/onsenui

--- bindings/react/src/components/BasicComponent.jsx
+++ bindings/react/src/components/BasicComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ons from 'onsenui';
+import ons from '@xh/onsenui';
 
 class BasicComponent extends React.Component {
   constructor(...args) {
